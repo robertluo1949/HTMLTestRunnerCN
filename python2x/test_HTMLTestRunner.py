1de34f8e625fe3e8e76a5b22b26e628a7538b4ea
Robert <robert_luo1949@163.com>
2017-08-28 01:13:02 +0800

    add testreport
    add testreport

---
+++ python2x/test_HTMLTestRunner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import unittest
+import HTMLTestRunner
+class testadd(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_add1(self):
+        self.assertEqual(2+3+5,10)
+    def test_add2(self):
+        self.assertEqual(0+8+7,15)
+    def tearDown(self):
+        pass
+def suite():
+    suiteTest=unittest.TestSuite()
+    suiteTest.addTest(testadd("test_add1"))
+    suiteTest.addTest(testadd("test_add2"))
+    return suiteTest
+if __name__=="__main__":
+    filepath='\.\\pyresult.html'
+    fp=file(filepath,'wb')
+    #定义测试报告的标题与描述
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'我是测试报告的标题',description=u'我是测试报告的描述')
+    runner.run(suite())
+    fp.close()
